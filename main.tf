a355a09df580ec7bec150a4dd0f9c0feace54f95
TodTete <vallejoricardo3@gmail.com>
2026-05-13 21:24:09 -0600

    tags

--- main.tf
+++ main.tf
@@ -34,10 +34,18 @@ resource "aws_instance" "nginx-sever" {
 resource "aws_key_pair" "nginx-server-ssh" {
   key_name = "nginx-server-ssh"
   public_key = file("nginx-server.key.pub")
+
+    tags = {
+    Name = "nginx-server-sg"
+    Environment = "test"
+    Owner = "TodTete"
+    Team = "DevOps"
+    Project = "Terraform Init"
+  }
 }
 
 ####### SECURE GROUPS #######
-resource "aws_security_group" "nginx-server-sg"{
+resource "aws_security_group" "nginx-server-sg" {
   name  = "nginx-server-sg"
   description = "Security group allowing SSH and HTTP access to the nginx server"
 
@@ -61,4 +69,13 @@ resource "aws_security_group" "nginx-server-sg"{
     protocol    = "-1" # Allow all outbound traffic
     cidr_blocks = ["0.0.0.0/0"] # Allow all outbound traffic
   }
+
+  tags = {
+    Name = "nginx-server-sg"
+    Environment = "test"
+    Owner = "TodTete"
+    Team = "DevOps"
+    Project = "Terraform Init"
+  }
 }
+
