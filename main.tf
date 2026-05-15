ef6147a4baca76f555dab467303337e24ae61113
TodTete <vallejoricardo3@gmail.com>
2026-05-15 12:37:15 -0600

    qa

--- main.tf
+++ main.tf
@@ -9,6 +9,15 @@ module "nginx_server_dev" {
     environment = "dev"
 }
 
+module "nginx_server_qa" {
+    source = "./nginx_server_modules"
+
+    ami_id = "ami-0440d3b780d96b29d"
+    instance_type = "t3.large"
+    server_name = "nginx-server-qa"
+    environment = "qa"
+}
+
 ####### OUTPUTS #######
 
 output "nginx_dev_ip" {
@@ -19,4 +28,14 @@ output "nginx_dev_ip" {
 output "nginx_dev_dns" {
   description = "The public DNS name of the nginx server"
   value       = module.nginx_server_dev.server_public_dns
+}
+
+output "nginx_qa_ip" {
+  description = "The public IP address of the nginx server"
+  value       = module.nginx_server_qa.server_public_ip
+}
+
+output "nginx_qa_dns" {
+  description = "The public DNS name of the nginx server"
+  value       = module.nginx_server_qa.server_public_dns
 }
